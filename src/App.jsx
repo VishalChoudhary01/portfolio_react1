eb206f555571fcc0ac906cedc207c49a9feba013
Vishal <vishal.bhumca21@gmail.com>
2024-07-22 22:52:48 +0530

    overflow hidden on y

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-    <section className='overflow-x-hidden text-neutral-300 antialiased selection:bg-sky-300 selection:text-[#222]'>
+    <section className='overflow-x-hidden overflow-y-hidden text-neutral-300 antialiased selection:bg-sky-300 selection:text-[#222]'>
       <section className='fixed top-0 -z-10 h-full w-full'>
 
     <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
