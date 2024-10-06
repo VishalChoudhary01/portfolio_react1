d7b91755f82a5ae1e7296c26ea93bd2a8be72cfd
Vishal <vishal.bhumca21@gmail.com>
2024-10-06 10:39:53 +0530

    logo changes

--- src/Component/Logo.jsx
+++ src/Component/Logo.jsx
@@ -3,8 +3,8 @@ const Logo = () => {
   return (
     <div>
     <section className=' lg:w-20 w-14 logoContainer'>
-        <section className='mx-2 lg:w-[55px] logoSquare w-[38px] h-[38px] md:w-[45px] md:h-[45px] flex items-center relative lg:h-[55px] lg:border-[4px] border-4  rounded-sm border-neutral-400'>
-        <p className='absolute lg:-left-6 md:-left-4 -left-4 font-semibold font-[RobotoSlab]  text-sm md:text-lg lg:text-2xl'>V <span className='tracking-wider'>Kr.</span></p>
+        <section className='mx-2 lg:w-[60px] logoSquare w-[38px] h-[38px] md:w-[45px] md:h-[45px] flex items-center relative lg:h-[55px] lg:border-[4px] border-4  rounded-sm border-neutral-400'>
+        <p className='absolute lg:-left-6 md:-left-4 -left-4 font-semibold font-[RobotoSlab]  text-sm md:text-lg lg:text-2xl'>V <span className='tracking-widest'>&nbsp;Kr.</span></p>
         </section>
     </section>
     </div>
